0b386ba20ebbc45bdc0f1c219b079e3ef0ce8bd6
Satoshi.M <st0326s@yahoo.co.jp>
2013-11-25 16:36:33 +0900

    CodeIQ問題
    フィボナッチ数列のうち、各桁の数字を足した数で割り切れる数を以下の例に続けて、
    小さい方から5個求めてください。
    
    例）
    2 : 2 ÷ 2
    3 : 3 ÷ 3
    5 : 5 ÷ 5
    8 : 8 ÷ 8
    21 : 21 ÷ 3　←　(2 + 1 = 3で割る)
    144 : 144 ÷ 9　←　(1 + 4 + 4 = 9で割る)
    
    【解答方法】
    解答フォームの1行目にカンマ区切りで答えを記入してください。（例：2,3,5,8,21）
    2行目以降は、あなたが求めた方法を記入してください。プログラムを書いた場合はコードを貼り付けてください。

---
+++ CodeIQ/Fibo/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            long befnum = 0;
+            long nownum = 1;
+            int nowMatchCnt = 0;
+
+            while (nowMatchCnt < 10)
+            {
+                long fibo = befnum + nownum;
+                int fiboLen = fibo.ToString().Length;
+                int divideNum = 0;
+
+                while (fiboLen > 0)
+                {
+                    divideNum = divideNum + int.Parse( fibo.ToString().Substring(fiboLen-1, 1));
+                    fiboLen--;
+                    
+                    
+                }
+
+//                System.Diagnostics.Debug.WriteLine("divideNum=" + divideNum.ToString());
+
+                if (fibo > 1)
+                {
+                    if (fibo % divideNum == 0)
+                    {
+                        nowMatchCnt++;
+                        System.Diagnostics.Debug.WriteLine("nowMatchCnt=" + nowMatchCnt.ToString());
+                        System.Diagnostics.Debug.WriteLine("fibo=" + fibo.ToString());
+                    }
+                }
+
+                befnum = nownum;
+                nownum = fibo;
+
+
+            }
+
+
+        }
+    }
+}
